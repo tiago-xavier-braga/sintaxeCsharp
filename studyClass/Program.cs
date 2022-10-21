679030b858cfc3f72b34b51a577d32b27183599b
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-10-21 08:53:04 -0300

    create classes

---
+++ studyClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace studyClass{
+    class Player{
+        public int id;
+        public string name;
+        public int level;
+
+        public void createObject(){
+            Console.WriteLine($"Name: {name} Level:{level}");
+        } 
+    }
+    class Program{
+        static void Main(){
+            Player p1 = new Player();
+            p1.id = 1;
+            p1.level = 12;
+            p1.name = "Tiago";
+            p1.createObject();
+        }
+    }
+}
