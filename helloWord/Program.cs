60fc6645aa5cf9e294e08cf256a32a490b6c08de
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-10-20 21:24:43 -0300

    first commit

---
+++ helloWord/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace helloWord
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("hello word!");
+        }
+    }
+}
---
+++ variable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace variables{
+    class Program{
+        static void Main(){
+            Console.WriteLine("Variables");
+
+            int x = 5;
+            Console.WriteLine(x);
+            x = 2;
+            Console.WriteLine(x);
+
+            string text = "Hello Word";
+            Console.WriteLine(text);
+
+            /*
+            string readNumberUser; 
+            readNumberUser = Console.ReadLine();
+            Console.WriteLine(readNumberUser);
+            */
+            Console.WriteLine("Cast");
+            int a = 2;
+            int b = 3;
+
+            float result = (float) a / b;
+            Console.WriteLine(result);
+            Console.WriteLine($"Result a/b is: {result}");
+        }
+    }
+}
